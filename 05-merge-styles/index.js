c6a3c0c782454070f7ad2da09e1617cd0973a0de
Andrei Bobryk <andreibobryk1@gmail.com>
2022-05-23 23:20:44 +0300

    refactor: task 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 const fsPromise = fs.promises;
 
-const pathDirSrcBundle = path.join(__dirname, "styles");
-const pathDirDestBundle = path.join(__dirname, "project-dist", "bundle.css");
+const pathDirSrcBundle = path.join(__dirname, 'styles');
+const pathDirDestBundle = path.join(__dirname, 'project-dist', 'bundle.css');
 
 const bundleCSS = (source, destination) =>
   fsPromise
-    .writeFile(destination, "")
+    .writeFile(destination, '')
     .catch(console.error)
     .then(
       fs.readdir(source, { withFileTypes: true }, (err, files) => {
@@ -15,9 +15,9 @@ const bundleCSS = (source, destination) =>
           console.log(err);
         } else {
           files.forEach((file) => {
-            if (file.isFile() && file.name.slice(-4) === ".css") {
+            if (file.isFile() && file.name.slice(-4) === '.css') {
               const pathSrc = path.join(source, file.name);
-              fs.readFile(pathSrc, "utf8", (err, data) => {
+              fs.readFile(pathSrc, 'utf8', (err, data) => {
                 if (err) throw err;
 
                 fsPromise.appendFile(destination, data, (err) => {
