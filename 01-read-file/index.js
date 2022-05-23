20d1c5ebef81b1ba96992be55ab22c4fb27276ea
Andrei Bobryk <andreibobryk1@gmail.com>
2022-05-23 22:38:02 +0300

    fix: task1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,15 +1,15 @@
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 
 const file = path.join(__dirname, 'text.txt');
 
 async function print(readable) {
-  readable.setEncoding("utf8");
-  let data = "";
+  readable.setEncoding('utf8');
+  let data = '';
   for await (const chunk of readable) {
     data += chunk;
   }
-  return data;
+  console.log(data);
 }
 
 print(fs.createReadStream(file)).catch(console.error);
