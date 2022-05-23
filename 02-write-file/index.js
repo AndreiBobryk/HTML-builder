167840867beb11711af8e0d7dab691812d74909d
Andrei Bobryk <andreibobryk1@gmail.com>
2022-05-23 22:45:56 +0300

    refactor: task2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,22 +1,22 @@
-const { stdout, stdin, exit } = process;
-const path = require("path");
-const fs = require("fs");
-const readline = require("readline");
+const { stdout, stdin } = process;
+const path = require('path');
+const fs = require('fs');
 
-stdout.write("Hi\n");
-const file = path.join(__dirname, "result.txt");
+
+stdout.write('Please, enter text:\n');
+const file = path.join(__dirname, 'result.txt');
 const output = fs.createWriteStream(file);
 
-process.on("exit", () => stdout.write("Удачи!"));
+process.on('exit', () => stdout.write('Good luck!'));
 
-stdin.on("data", (chunk) => {
-  if (chunk.includes("exit")) {
+stdin.on('data', (chunk) => {
+  if (chunk.includes('exit')) {
     process.exit();
   }
   output.write(chunk);
 });
-stdin.on("error", (error) => console.log("Error", error.message));
+stdin.on('error', (error) => console.log('Error', error.message));
 
-process.on("SIGINT", () => {
+process.on('SIGINT', () => {
   process.exit(0);
 });
